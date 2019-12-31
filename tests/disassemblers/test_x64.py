9ef69b1aef5ba438f74270eedfc689424d285805
Chris Collins <chriscoll93@gmail.com>
2019-12-31 05:06:11 +0000

    Wrote the first few tests for the add instruction. A bug was found that needs to be fixed.

--- tests/disassemblers/test_x64.py
+++ tests/disassemblers/test_x64.py
@@ -1,9 +1,66 @@
 import pytest
+import re
 
+from pyda.binaries.binary import Function
 from pyda.disassemblers.x64.asm import disassemble
+from pyda.disassemblers.x64.definitions import *
 
 class TestX64():
 
+    def helper( self, mnemonic, src, dst, assembly ):
+
+        # Create a new function with the assembly and disassemble it
+        function = Function(name='testFunc', addr=0, size=0, assembly=assembly)
+        assemblyRe = re.compile(r'0:\s+{opcode:02x}\s+{modRm:02x}\s+{mnemonic}\s+{src},\s+{dst}'.format(
+            opcode=assembly[0], modRm=assembly[1], mnemonic=mnemonic, src=re.escape(src), dst=re.escape(dst)))
+
+        disassemble(function)
+
+        # Print the string just in case the test fails so it can be seen
+        assemblyString = "{}".format(function.instructions[0])
+        print(assemblyString)
+        match = assemblyRe.match(assemblyString)
+
+        # Return the function for analysis and the regex match object
+        return function, match
+
+
     def test_add_no_immediate( self ):
 
-        assert 1 == 2
+        ###############################################
+        #  1 BYTE OPERANDS, MOD R/M BYTE IS FOR DEST  #
+        ###############################################
+
+        #           Address mode | source         | destination
+        modRmByte = MOD_INDIRECT | (REG_RCX << 3) | REG_RAX
+        assembly = bytes([0x00, modRmByte])
+        function, match = self.helper("add", "%cl", "[%rax]", assembly)
+        assert len(function.instructions) == 1
+        assert match is not None
+
+        ###############################################
+        #  4 BYTE OPERANDS, MOD R/M BYTE IS FOR DEST  #
+        ###############################################
+
+        #           Address mode | source         | destination
+        modRmByte = MOD_INDIRECT | (REG_RCX << 3) | REG_RAX
+        assembly = bytes([0x01, modRmByte])
+        function, match = self.helper("add", "%ecx", "[%rax]", assembly)
+        assert len(function.instructions) == 1
+        assert match is not None
+
+        ##############################################
+        #  1 BYTE OPERANDS, MOD R/M BYTE IS FOR SRC  #
+        ##############################################
+
+        #           Address mode | destination    | source
+        modRmByte = MOD_INDIRECT | (REG_RCX << 3) | REG_RAX
+        assembly = bytes([0x02, modRmByte])
+        function, match = self.helper("add", "%[rax]", "%cl", assembly)
+        assert len(function.instructions) == 1
+        assert match is not None
+
+
+
+
+
