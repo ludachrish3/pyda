cd6381f991375e1a9853a81eddaa98c29f20943a
Chris Collins <chriscoll93@gmail.com>
2019-12-31 13:39:22 -0600

    Fixed tests so that any number of bytes can be sent to the helper function.

--- tests/disassemblers/test_x64.py
+++ tests/disassemblers/test_x64.py
@@ -13,8 +13,9 @@ class TestX64():
 
         # Create a new function with the assembly and disassemble it
         function = Function(name='testFunc', addr=0, size=0, assembly=assembly)
-        assemblyRe = re.compile(r'0:\s+{opcode:02x}\s+{modRm:02x}\s+{mnemonic}\s+{src},\s+{dst}'.format(
-            opcode=assembly[0], modRm=assembly[1], mnemonic=mnemonic, src=re.escape(src), dst=re.escape(dst)))
+        assemblyRe = re.compile(r'0:\s+{assembly}\s+{mnemonic}\s+{src},\s+{dst}'.format(
+            assembly=" ".join(["{:02x}".format(x) for x in list(assembly)]),
+            mnemonic=mnemonic, src=re.escape(src), dst=re.escape(dst)))
 
         disassemble(function)
 
@@ -28,9 +29,9 @@ class TestX64():
 
     def test_add_no_immediate( self ):
 
-        ###############################################
-        #  1 BYTE OPERANDS, MOD R/M BYTE IS FOR DEST  #
-        ###############################################
+        ##########################################################
+        #  OPCODE 00: 1 BYTE OPERANDS, MOD R/M BYTE IS FOR DEST  #
+        ##########################################################
 
         #           Address mode | source         | destination
         modRmByte = MOD_INDIRECT | (REG_RCX << 3) | REG_RAX
@@ -39,9 +40,9 @@ class TestX64():
         assert len(function.instructions) == 1
         assert match is not None
 
-        ###############################################
-        #  4 BYTE OPERANDS, MOD R/M BYTE IS FOR DEST  #
-        ###############################################
+        ##########################################################
+        #  OPCODE 01: 4 BYTE OPERANDS, MOD R/M BYTE IS FOR DEST  #
+        ##########################################################
 
         #           Address mode | source         | destination
         modRmByte = MOD_INDIRECT | (REG_RCX << 3) | REG_RAX
@@ -50,9 +51,9 @@ class TestX64():
         assert len(function.instructions) == 1
         assert match is not None
 
-        ##############################################
-        #  1 BYTE OPERANDS, MOD R/M BYTE IS FOR SRC  #
-        ##############################################
+        #########################################################
+        #  OPCODE 02: 1 BYTE OPERANDS, MOD R/M BYTE IS FOR SRC  #
+        #########################################################
 
         #           Address mode | destination    | source
         modRmByte = MOD_INDIRECT | (REG_RCX << 3) | REG_RAX
@@ -61,9 +62,9 @@ class TestX64():
         assert len(function.instructions) == 1
         assert match is not None
 
-        ##############################################
-        #  4 BYTE OPERANDS, MOD R/M BYTE IS FOR SRC  #
-        ##############################################
+        #########################################################
+        #  OPCODE 03: 4 BYTE OPERANDS, MOD R/M BYTE IS FOR SRC  #
+        #########################################################
 
         #           Address mode | destination    | source
         modRmByte = MOD_INDIRECT | (REG_RCX << 3) | REG_RAX
@@ -75,9 +76,9 @@ class TestX64():
 
     def test_add_immediate( self ):
 
-        ###############################
-        #  1 BYTE IMMEDIATE INTO %AL  #
-        ###############################
+        ##########################################
+        #  OPCODE 04: 1 BYTE IMMEDIATE INTO %AL  #
+        ##########################################
 
         immediate = 0x08
         assembly = bytes([0x04, immediate])
@@ -85,9 +86,9 @@ class TestX64():
         assert len(function.instructions) == 1
         assert match is not None
 
-        ################################
-        #  1 BYTE IMMEDIATE INTO %EAX  #
-        ################################
+        ###########################################
+        #  OPCODE 05: 1 BYTE IMMEDIATE INTO %EAX  #
+        ###########################################
 
         assembly = bytes([0x05, 0x08, 0x00, 0x00, 0x00])
         function, match = self.helper("add", "0x8", "%eax", assembly)
