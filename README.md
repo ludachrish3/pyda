cc77a36299f992c1052b8fb07ab298cf1734ba4c
Chris Collins <chriscoll93@gmail.com>
2020-08-30 22:38:16 -0500

    Added the skeleton of a text-based user interface (TUI)

--- README.md
+++ README.md
@@ -17,19 +17,19 @@ optional arguments:
 
 ## TODO
 
+* Add a user interface that take input to print certain things, like all symbols, functions, global variables, instructions in a function
+* Add command line options for getting just some header info like objdump and readelf does. This should be pretty easy, and can be made more user friendly by using human readable strings.
+* Add semantics to instructions at the base level. There should be types of instructions, like add, subtract, multiply, call function, pop stack push stack, type conversion, etc
 * Make comparison instructions automatically set the destination to the REG_RFLAGS register
 * Create a fixup function that copies destination into source when needed so that operands are explicit (mainly for just math?) Need to still have correct syntax in Intel and AT&T format.
 * Support AT&T assembly syntax so that it can be diffed against objdump output
 * Support Intel assembly syntax
 * Change relative jumps to just have a value of REG_RIP, and then mark the operand as indirect and set its displacement if the operand's value is REG_RIP. Whether the operand is signed will be determined by whether the operand is indirect.
 * In order to find functions in a stripped binary, start at beginning of .text section. Start disassembling and consider all jumps that can be taken. If there are no jumps left and a ret instruction is reached, then that is the end of the function. All instructions after it (not including NOPs) are another function.
-* Assign instruction types for each X64InstructionInfo object. This can implicitly hold properties that are always true for a type of instruction, like for exchanges and floating point instructions.
 * Update tests
 * Need to work on symbol resolution first so that more information is available for determining when functions end. For example, knowing when exit is called is a good way to indicate a possible end of function, like return would.
-* Mark each function symbol when creating them for whether they should be disassembled or not. This is useful in cases where the symbol says the size is 0 bytes when it can actually be disassembled, like \_init()
 * Use function sizes as more of a hint instead of a definite size. If it is nonzero, it's okay to trust it. If it's 0, then using the logic for finding functions in a stripped binary can be used to determine when the function ends. In this case, keeping the memory mapped file around is needed. This means that keeping the "assembly" field around for function symbols is unnecessary.
 * Figure out which function is main() by figuring out which address is passed to \__libc_start_main() (can only be done after some basic decompiling is done)
-* Add command line options for getting just some header info like objdump and readelf does. This should be pretty easy, and can be made more user friendly by using human readable strings.
 * Take alignment into account when looking for functions in a stripped binary. Functions need to start at an offset where address % alignment == 0
 * Change error handling to be try blocks and raising exceptions instead of checking return values. Checking handleOperandAddressing() needs to be done in the case that an invalid segment register is used.
 * Get 100% code coverage in tests (or close to it)
--- pyda/pyda.py
+++ pyda/pyda.py
@@ -7,7 +7,7 @@ Description:    This file is the CLI frontend for interacting with the pyda
 
 from argparse import ArgumentParser
 import traceback
-import os
+import os, sys
 
 from pyda.exeParsers import exeParser
 from pyda.decompilers import decompiler
@@ -15,6 +15,86 @@ from pyda.decompilers import decompiler
 import logging
 
 logger = logging.getLogger(__package__)
+
+helpMessage = """
+[l]ist - list the available things
+[s]how - show a function
+[h]elp - show this help output
+[e]xit - exit the program (same as quit)
+[q]uit - quit the program (same as exit)
+"""
+
+listHelpMessage = """
+list [f]unctions        - lists the names of all functions
+list [g]lobal variables - lists the names of all global variables
+list [s]ymbols          - lists all symbols and their info
+"""
+
+showHelpMessage = """
+show [[a]ssembly] <function name> - shows a function's code, or its assembly if
+                                    "assembly" is also provided
+"""
+
+
+def runTui( executables ):
+    """
+    Description:    Work loop to take user input and print the desired info
+
+    Arguments:      executables - List of Executable objects to interact with
+
+    Return:         None
+    """
+
+    prompt = "pyda> "
+
+    listCommands = ["l", "list"]
+    showCommands = ["s", "show"]
+    exitCommands = ["q", "e", "quit", "exit"]
+    helpCommands = ["h", "help"]
+
+    # TODO: Support multiple executables that are imported together
+    executable = executables[0]
+
+    while True:
+
+        print(f"{prompt}", end='')
+        line = input().strip().lower()
+
+        tokens = line.split(" ")
+
+        if tokens[0] in exitCommands:
+            break
+
+        elif tokens[0] in helpCommands:
+
+            # Show the main help if no specific command is chosen
+            if len(tokens) == 1:
+
+                print(f"{helpMessage}")
+
+            elif len(tokens) == 2 and tokens[1] in listCommands:
+
+                print(f"{listHelpMessage}")
+
+            elif len(tokens) == 2 and tokens[1] in showCommands:
+
+                print(f"{showHelpMessage}")
+
+            else:
+
+                print(f"'{tokens[1]}' is not a valid command")
+
+        elif tokens[0] in listCommands:
+
+            print("listing things")
+
+        elif tokens[0] in showCommands:
+
+            print("showing things")
+
+        else:
+            print(f"'{tokens[0]}' is not a valid command")
+
 
 def main():
 
@@ -30,7 +110,7 @@ def main():
     required.add_argument("file", nargs="+", help="the binary file(s) to analyze")
 
     optional = parser.add_argument_group("optional arguments")
-    optional.add_argument("-l", "--log-level", choices=debugLevels, default="INFO", type=str.upper, metavar="", help="Log level to use when printing logs")
+    optional.add_argument("-l", "--log-level", choices=debugLevels, default="WARNING", type=str.upper, metavar="", help="Log level to use when printing logs")
     optional.add_argument("-h", "--help", action="help", help="Show this help message and exit")
 
     args = parser.parse_args()
@@ -43,6 +123,8 @@ def main():
     logger.addHandler(logHandler)
     logger.setLevel(args.log_level)
 
+    executables = []
+
     # TODO: Add support for handling multiple files together and link them
     # against each other.
     for filename in args.file:
@@ -52,12 +134,15 @@ def main():
             exit(1)
 
         try:
-            executable = exeParser.parseExe(filename)
+            executables.append(exeParser.parseExe(filename))
 
         except Exception as e:
             logger.error(f"An error occurred while parsing the executable: {e}")
             traceback.print_tb(e.__traceback__)
             exit(1)
+
+    runTui(executables)
+
 
 if __name__ == "__main__":
 
